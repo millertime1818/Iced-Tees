cda60a55b339dc2029024fb6acfc445e51526ad2
xavieresp1 <102778178+xavieresp1@users.noreply.github.com>
2022-05-02 11:08:16 -0400

    This is the Social Page prototype

---
+++ Social Page/Social Page/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Social_Page
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MinimumSize = new System.Drawing.Size(500, 700);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(500, 700);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(573, 50);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(133, 63);
+            this.loadBtn.TabIndex = 1;
+            this.loadBtn.Text = "Load Posts";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            this.loadBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.loadBtn_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(986, 676);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button loadBtn;
+    }
+}
+
---
+++ Social Page/Social Page/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Social_Page
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //LV properties
+            listView1.View = View.Details;
+            //construct columns
+            listView1.Columns.Add("designs", 250);
+        }
+        //populate with images
+        private void populate()
+        {
+            //img list to hold images
+            ImageList imgs = new ImageList();
+            imgs.ImageSize = new Size(50, 50);
+            
+            //load images from file
+            String[] paths = { };
+            paths = Directory.GetFiles(@"C:Iced Tees Project\Website\Social Page\bin\Debug\Designs");
+
+            foreach(string path in paths)
+            {
+               imgs.Images.Add(Image.FromFile(path));
+            }
+            
+            listView1.SmallImageList = imgs;
+            listView1.Items.Add("SocraTees16", 0);
+            listView1.Items.Add("MrTees", 1);
+            listView1.Items.Add("ArisTeestle4", 2);
+            listView1.Items.Add("MrTeeser", 3);
+            listView1.Items.Add("novelTees78", 4);
+            listView1.Items.Add("GWashingTees86", 5);
+            listView1.Items.Add("MegaTdesigns", 6);
+            listView1.Items.Add("User81234", 7);
+            listView1.Items.Add("BostonTeesParty32", 8);
+            listView1.Items.Add("xXTeesGuyXx", 9);
+        }
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void loadBtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            populate();
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            String selected = listView1.SelectedItems[0].SubItems[0].Text;
+             
+            MessageBox.Show(selected);
+           
+        }
+    }
+}
