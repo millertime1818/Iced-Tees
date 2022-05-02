16352fd296fe6d035f4a2098d020b88d406b0ad3
millertime1818 <95248442+millertime1818@users.noreply.github.com>
2022-05-02 19:31:37 -0400

    Add files via upload

---
+++ Website/Log-in system/Log-in system/Log in form.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Log_in_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Loginbutton = new System.Windows.Forms.Button();
+            this.errorMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(43, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome To Iced Tees! \r\nPlease enter your username\r\nand password";
+            // 
+            // userNbox
+            // 
+            this.userNbox.Location = new System.Drawing.Point(47, 228);
+            this.userNbox.Name = "userNbox";
+            this.userNbox.Size = new System.Drawing.Size(276, 26);
+            this.userNbox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(47, 315);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(276, 26);
+            this.passBox.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 490);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 68);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Create a new account";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(474, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 344);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Loginbutton
+            // 
+            this.Loginbutton.Location = new System.Drawing.Point(47, 418);
+            this.Loginbutton.Name = "Loginbutton";
+            this.Loginbutton.Size = new System.Drawing.Size(213, 58);
+            this.Loginbutton.TabIndex = 9;
+            this.Loginbutton.Text = "Log In";
+            this.Loginbutton.UseVisualStyleBackColor = true;
+            this.Loginbutton.Click += new System.EventHandler(this.Loginbutton_Click);
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.Location = new System.Drawing.Point(47, 377);
+            this.errorMessage.MinimumSize = new System.Drawing.Size(300, 20);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(300, 20);
+            this.errorMessage.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1084, 641);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.Loginbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userNbox);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(1106, 697);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Loginbutton;
+        private System.Windows.Forms.Label errorMessage;
+    }
+}
+
---
+++ Website/Log-in system/Log-in system/Log in form.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Log_in_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void usernameCheck(string username)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            New_user_form new_user_form = new New_user_form();
+            new_user_form.ShowDialog();
+            this.Close();
+        }
+
+        private void Loginbutton_Click(object sender, EventArgs e)
+        {
+            string userandpass = userNbox.Text + "," + passBox.Text;
+            {
+                if (File.Exists("C:/Users/mille/Python/Iced Tees Project/Website/Log-in system/bin/Debug/UNP.txt")) // If file exists
+                {
+                    if (File.ReadAllText("C:/Users/mille/Python/Iced Tees Project/Website/Log-in system/bin/Debug/UNP.txt").Contains(userandpass))
+                    {
+                        errorMessage.Text = "Successful login";
+                        this.Hide();
+                        successful_login successful_login_form = new successful_login();
+                        successful_login_form.ShowDialog();
+                        this.Close();
+                    }
+                    else
+                    {
+                        errorMessage.Text = "username or password is wrong";
+                    }
+                }
+                else
+                {
+                    errorMessage.Text = "Could not check usernames and passwords";
+                }
+              
+            }
+        }
+
+    }
+}
